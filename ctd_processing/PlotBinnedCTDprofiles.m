4267f2119ff592b8f0686c0b5a2b70229410a062
andypicke <andypicke@gmail.com>
2015-10-15 15:28:53 -0600

    updated figure
    Plot up and down casts separately now. Before, could not tell if there
    was data missing from one because they were on top of each other.

--- ctd_processing/PlotBinnedCTDprofiles.m
+++ ctd_processing/PlotBinnedCTDprofiles.m
@@ -1,33 +1,61 @@
 function h=PlotBinnedCTDprofiles(datad_1m,datau_1m,ctdlist,icast)
 %%
 h=figure;clf
-subplot(121)
-plot(datad_1m.t1,datad_1m.p,'-')
+agutwocolumn(1)
+wysiwyg
+subplot(221)
+plot(datad_1m.t1,datad_1m.p,'s-')
 hold on
-plot(datad_1m.t2,datad_1m.p,'--')
-plot(datau_1m.t1,datau_1m.p,'-')
-hold on
-plot(datau_1m.t2,datau_1m.p,'--')
+plot(datad_1m.t2,datad_1m.p,'o-')
+%plot(datau_1m.t1,datau_1m.p,'-')
+%hold on
+%plot(datau_1m.t2,datau_1m.p,'--')
 
 axis ij
 grid on
 xlabel('Temp [^oC]')
 ylabel('Pressure [db]')
 title(ctdlist(icast).name,'interpreter','none')
-legend('t1 down','t2 down','t1 up','t2 up','location','Southeast')
+%legend('t1 down','t2 down','t1 up','t2 up','location','Southeast')
+legend('t1 down','t2 down','location','Southeast')
 
-subplot(122)
-plot(datad_1m.s1,datad_1m.p,'-')
+subplot(222)
+%plot(datad_1m.t1,datad_1m.p,'-')
+%hold on
+%plot(datad_1m.t2,datad_1m.p,'--')
+plot(datau_1m.t1,datau_1m.p,'s-')
 hold on
-plot(datad_1m.s2,datad_1m.p,'--')
-plot(datau_1m.s1,datau_1m.p,'-')
+plot(datau_1m.t2,datau_1m.p,'o-')
+legend('t1 up','t2 up','location','Southeast')
+axis ij
+grid on
+xlabel('Temp [^oC]')
+
+subplot(223)
+plot(datad_1m.s1,datad_1m.p,'s-')
 hold on
-plot(datau_1m.s2,datau_1m.p,'--')
+plot(datad_1m.s2,datad_1m.p,'o-')
+%plot(datau_1m.s1,datau_1m.p,'-')
+%hold on
+%plot(datau_1m.s2,datau_1m.p,'--')
 axis ij
 grid on
 xlabel('Sal.')
 ylabel('Pressure [db]')
-legend('s1 down','s2 down','s1 up','s2 up','location','best')
+legend('s1 down','s2 down','location','best')
+
+subplot(224)
+%plot(datad_1m.s1,datad_1m.p,'-')
+%hold on
+%plot(datad_1m.s2,datad_1m.p,'--')
+plot(datau_1m.s1,datau_1m.p,'s-')
+hold on
+plot(datau_1m.s2,datau_1m.p,'o-')
+axis ij
+grid on
+xlabel('Sal.')
+%ylabel('Pressure [db]')
+legend('s1 up','s2 up','location','best')
 
 return
 %%

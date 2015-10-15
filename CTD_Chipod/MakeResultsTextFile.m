f9dcbdd1afdfe44ce78b824fc28c60f67cc891e8
andypicke <andypicke@gmail.com>
2015-10-15 15:28:14 -0600

    New
    Replace a bunch of code that is common to all CTD-chipod processing
    scripts.

---
+++ CTD_Chipod/MakeResultsTextFile.m
@@ -0,0 +1,35 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% MakeResultsTextFile.m
+%
+% Initialize a text file for results of CTD-chipod processing
+%
+% Replaces a bunch of l ines in processing script (deletes any fles of same
+% name already existing in chi_proc_path).
+% 
+%---------------
+% 10/7/15 - AP - apickering@coas.oregonstate.edu - initial coding
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+% make a text file to print a summary of results to
+txtfname=['Results' datestr(floor(now)) '.txt'];
+
+if exist(fullfile(chi_proc_path,txtfname),'file')
+%    delete(fullfile(chi_proc_path,txtfname))
+end
+
+fileID= fopen(fullfile(chi_proc_path,txtfname),'a');
+fprintf(fileID,['\n \n CTD-chipod Processing Summary\n']);
+fprintf(fileID,['\n \n Created ' datestr(now) '\n']);
+fprintf(fileID,'\n CTD path \n');
+fprintf(fileID,[CTD_out_dir_root '\n']);
+fprintf(fileID,'\n Chipod data path \n');
+fprintf(fileID,[chi_data_path '\n']);
+fprintf(fileID,'\n Chipod processed path \n');
+fprintf(fileID,[chi_proc_path '\n']);
+%fprintf(fileID,'\n figure path \n');
+%fprintf(fileID,[chi_fig_path '\n \n']);
+fprintf(fileID,[' \n There are ' num2str(length(CTD_list)) ' CTD files' ]);
+
+%%

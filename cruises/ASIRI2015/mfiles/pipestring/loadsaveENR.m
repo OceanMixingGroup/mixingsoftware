f0aa0afefa2edb1f7e7b4b73d82c72a249985684
andypicke <andypicke@gmail.com>
2015-10-21 11:15:52 -0600

    remove old code

--- cruises/ASIRI2015/mfiles/pipestring/loadsaveENR.m
+++ cruises/ASIRI2015/mfiles/pipestring/loadsaveENR.m
@@ -29,8 +29,6 @@ MfilePath='/Users/Andy/Cruises_Research/mixingsoftware/cruises/ASIRI2015/mfiles/
 addpath(fullfile(MfilePath,'shared'))
 addpath(fullfile(MfilePath,'nav'))
 
-%addpath('/Volumes/scienceparty_share/mfiles/shared/')
-
 rawdir = fullfile(SciencePath,'pipestring','raw');
 savedir = fullfile(SciencePath,'pipestring','mat');
 cruise ='ASIRI2015';
@@ -52,7 +50,7 @@ for ifile=2:length(files)
     fname=files(ifile).name
     fname_beam=['ADCP_' fname(1:end-4) '_beam.mat'];
     fname_earth=['ADCP_' fname(1:end-4) '_earth.mat'];
-
+    
     clear adcp
     
     if ~exist(fullfile(savedir,fname_beam),'file')
@@ -75,7 +73,7 @@ for ifile=2:length(files)
         adcp=rmfield(adcp,'bt_corr');
         adcp=rmfield(adcp,'bt_ampl');
         adcp=rmfield(adcp,'bt_perc_good');
-                
+        
         save(fullfile(savedir,fname_beam),'adcp')
     else
         disp([fname_beam ' already exists'])
@@ -84,42 +82,42 @@ for ifile=2:length(files)
     if ~exist(fullfile(savedir,fname_earth),'file')
         
         % load beam velocity mat file
-%        if ~exist(adcp,'var')
+        %        if ~exist(adcp,'var')
         clear adcp
         disp('loading beam velocities')
         load(fullfile(savedir,fname_beam));
         
-        clear xadcp nadcp N       
+        clear xadcp nadcp N
         
         disp('loading nav data')
         N=loadNavSpecTime([nanmin(adcp.mtime) nanmax(adcp.mtime)]);
         if numel(N.head)>2
-        
-        ttemp_nav=N.dnum_hpr; ig=find(diff(ttemp_nav)>0); ig=ig(1:end-1)+1;
-        
-        adcp.config.orientation='down';
-        xadcp = adcp;
-        %         xadcp.east_vel = xadcp.v1;
-        %         xadcp.north_vel = xadcp.v2;
-        %         xadcp.vert_vel = xadcp.v3;
-        %         xadcp.error_vel = xadcp.v4;
-        clear ttemp ig
-        ttemp=N.dnum_hpr; ig=find(diff(ttemp)>0); ig=ig(1:end-1)+1;
-        xadcp.heading=interp1(ttemp(ig),N.head(ig),xadcp.mtime);
-        xadcp.pitch=interp1(ttemp(ig),N.pitch(ig),xadcp.mtime);
-        xadcp.roll=interp1(ttemp(ig),N.roll(ig),xadcp.mtime);
-        
-        xadcp.Info='head/pitch/roll from ship nav';
-        xadcp.MakeInfo=['Made ' datestr(now) ' w/ loadsaveENR.m'];
-        
-        disp(['rotating ' fname_beam ' from beam to earth'])
-        nadcp=beam2earth_workhorse(xadcp);
-        
-        nadcp.Info='head/pitch/roll from ship nav';
-        nadcp.MakeInfo=['Made ' datestr(now) ' w/ loadsaveENR.m'];
-        save(fullfile(savedir,fname_earth),'xadcp','nadcp')
+            
+            ttemp_nav=N.dnum_hpr; ig=find(diff(ttemp_nav)>0); ig=ig(1:end-1)+1;
+            
+            adcp.config.orientation='down';
+            xadcp = adcp;
+            %         xadcp.east_vel = xadcp.v1;
+            %         xadcp.north_vel = xadcp.v2;
+            %         xadcp.vert_vel = xadcp.v3;
+            %         xadcp.error_vel = xadcp.v4;
+            clear ttemp ig
+            ttemp=N.dnum_hpr; ig=find(diff(ttemp)>0); ig=ig(1:end-1)+1;
+            xadcp.heading=interp1(ttemp(ig),N.head(ig),xadcp.mtime);
+            xadcp.pitch=interp1(ttemp(ig),N.pitch(ig),xadcp.mtime);
+            xadcp.roll=interp1(ttemp(ig),N.roll(ig),xadcp.mtime);
+            
+            xadcp.Info='head/pitch/roll from ship nav';
+            xadcp.MakeInfo=['Made ' datestr(now) ' w/ loadsaveENR.m'];
+            
+            disp(['rotating ' fname_beam ' from beam to earth'])
+            nadcp=beam2earth_workhorse(xadcp);
+            
+            nadcp.Info='head/pitch/roll from ship nav';
+            nadcp.MakeInfo=['Made ' datestr(now) ' w/ loadsaveENR.m'];
+            save(fullfile(savedir,fname_earth),'xadcp','nadcp')
         else
-           disp(['no nav data for this time period, skipping transform']) 
+            disp(['no nav data for this time period, skipping transform'])
         end
         
     else
@@ -131,43 +129,3 @@ end
 delete(hb)
 
 %%
-
-%
-% filenames=[];
-% for i=1:length(files)-1; % last file is partial and doesn't completely load? skip it
-%     filenames{i}=char(files(i).name(end-13:end-4));
-% end
-%
-% % check which files have been processed already
-% kp=ones(1,length(filenames));
-% for i=1:length(files_processed);
-%     filenames_processed=char(files_processed(i).name(end-18:end-9));
-%     if sum(strcmp(filenames_processed,filenames))==1;
-%         kp(i)=0;
-%     end
-% end
-% %
-% ind=find(kp==1);
-% %%
-% if isempty(ind);
-%     kp(end)=1;
-% elseif kp(1)==1
-%     kp=kp;
-% else
-%     kp(ind(1)-1)=1;
-% end
-% %%
-% % read in the new files
-% files=files(kp==1);
-%
-% disp(['Reading in ' num2str(length(files)) ' new files '])
-% pause(1)
-% for i=1:length(files)
-%     [adcp,config]=rdradcp([rawdir files(i).name],1);
-%     adcp.mtime(adcp.mtime==0)=nan; %
-%     save([savedir 'ADCP_' files(i).name(1:end-4) '_beam.mat'],'adcp')
-% end
-%
-% %%
-%
-%

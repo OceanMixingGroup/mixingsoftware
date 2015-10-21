2aa8099ae322564d2deabf769ded65634d50521f
andypicke <andypicke@gmail.com>
2015-10-21 12:55:05 -0600

    add m-file with section names

---
+++ cruises/ASIRI2015/mfiles/cruise_report/fctd_sec_name_time.m
@@ -0,0 +1,130 @@
+%Section time indices
+
+fctd_names(1).name='jet1';  %sections names
+fctd_names(2).name='jet2';
+fctd_names(3).name='upnorth';
+fctd_names(4).name='eddy1';
+fctd_names(5).name='eddy2';
+fctd_names(6).name='duenorth';
+fctd_names(7).name='SEleg';
+fctd_names(8).name='SWleg';
+fctd_names(9).name='bigtriangle1';
+fctd_names(10).name='bigtriangle2';
+
+fctd_names(11).name='drifter1';
+fctd_names(12).name='drifterX1';
+fctd_names(13).name='drifter2';
+fctd_names(14).name='drifterX2';
+fctd_names(15).name='drifter3';
+fctd_names(16).name='dangler';
+
+fctd_names(17).name='nidhi1';
+fctd_names(18).name='nidhiX1';
+fctd_names(19).name='nidhi2';
+fctd_names(20).name='dangler2';
+fctd_names(21).name='nidhi3';
+fctd_names(22).name='nidhiX3';
+fctd_names(23).name='nidhi4';
+fctd_names(24).name='nidhiX4';
+fctd_names(25).name='nidhi5';
+fctd_names(26).name='nidhi6';
+fctd_names(27).name='nidhi7';
+fctd_names(28).name='nidhi8';
+fctd_names(29).name='nidhi9';
+fctd_names(30).name='nidhi10';
+fctd_names(31).name='nidhi11';
+fctd_names(32).name='nidhi12';
+fctd_names(33).name='nidhi13';
+fctd_names(34).name='front1';
+fctd_names(35).name='front2';
+fctd_names(36).name='front3';
+fctd_names(37).name='front4';
+fctd_names(38).name='front5';
+fctd_names(39).name='front6';
+fctd_names(40).name='front7';
+fctd_names(41).name='front8';
+fctd_names(42).name='front9';
+fctd_names(43).name='front10';
+fctd_names(44).name='front11';
+fctd_names(45).name='secHome1';
+
+
+fctd_names(1).st=datenum([2015 08 24 11 38 00]); %section start times
+fctd_names(2).st=datenum([2015 08 25 07 10 00]);
+fctd_names(3).st=datenum([2015 08 28 06 44 00]);
+fctd_names(4).st=datenum([2015 08 29 13 35 00]);
+fctd_names(5).st=datenum([2015 08 29 23 43 00]);
+fctd_names(6).st=datenum([2015 08 30 16 08 00]);
+fctd_names(7).st=datenum([2015 08 31 15 10 00]);
+fctd_names(8).st=datenum([2015 09 01 07 00 00]);
+fctd_names(9).st=datenum([2015 09 01 17 20 00]);
+fctd_names(10).st=datenum([2015 09 01 23 07 00]);
+fctd_names(11).st=datenum([2015 09 02 07 31 00]);
+fctd_names(12).st=datenum([2015 09 02 16 50 00]);
+fctd_names(13).st=datenum([2015 09 02 18 20 00]);
+fctd_names(14).st=datenum([2015 09 02 22 19 00]);
+fctd_names(15).st=datenum([2015 09 02 23 51 00]);
+fctd_names(16).st=datenum([2015 09 03 02 20 00]);
+fctd_names(17).st=datenum([2015 09 03 15 16 00]);
+fctd_names(18).st=datenum([2015 09 03 18 12 00]);
+fctd_names(19).st=datenum([2015 09 04 00 28 00]);
+fctd_names(20).st=datenum([2015 09 04 07 16 00]);
+fctd_names(21).st=datenum([2015 09 04 08 50 00]);
+fctd_names(22).st=datenum([2015 09 04 17 18 00]);
+fctd_names(23).st=datenum([2015 09 04 17 52 00]);
+fctd_names(24).st=datenum([2015 09 04 23 24 00]);
+fctd_names(25).st=datenum([2015 09 05 00 11 00]);
+
+
+fctd_names(1).et=datenum([2015 08 25 07 08 00]); %section end times
+fctd_names(2).et=datenum([2015 08 26 01 46 00]);
+fctd_names(3).et=datenum([2015 08 29 13 54 00]);
+fctd_names(4).et=datenum([2015 08 29 23 40 00]);
+fctd_names(5).et=datenum([2015 08 30 11 26 00]);
+fctd_names(6).et=datenum([2015 08 31 15 07 00]);
+fctd_names(7).et=datenum([2015 09 01 06 57 00]);
+fctd_names(8).et=datenum([2015 09 01 17 15 00]);
+fctd_names(9).et=datenum([2015 09 01 23 05 00]);
+fctd_names(10).et=datenum([2015 09 02 07 28 00]);
+fctd_names(11).et=datenum([2015 09 02 16 47 00]);
+fctd_names(12).et=datenum([2015 09 02 18 15 00]);
+fctd_names(13).et=datenum([2015 09 02 22 18 00]);
+fctd_names(14).et=datenum([2015 09 02 23 15 00]);
+fctd_names(15).et=datenum([2015 09 03 02 20 00]);
+fctd_names(16).et=datenum([2015 09 03 04 05 00]);
+fctd_names(17).et=datenum([2015 09 03 18 09 00]);
+fctd_names(18).et=datenum([2015 09 03 23 07 00]);
+fctd_names(19).et=datenum([2015 09 04 03 29 00]);
+fctd_names(20).et=datenum([2015 09 04 08 30 00]);
+fctd_names(21).et=datenum([2015 09 04 17 17 00]);
+fctd_names(22).et=datenum([2015 09 04 17 46 00]);
+fctd_names(23).et=datenum([2015 09 04 23 23 00]);
+fctd_names(24).et=datenum([2015 09 05 00 10 00]);
+fctd_names(25).et=datenum([2015 09 05 09 15 00]);
+
+
+fctd_names(26).st = 736212.483308102;       fctd_names(26).et = 736212.794509607;
+fctd_names(27).st = 736212.828477199;       fctd_names(27).et = 736213.100004861;
+fctd_names(28).st = 736213.138877894;       fctd_names(28).et = 736213.407136806;
+fctd_names(29).st = 736213.428902083;       fctd_names(29).et = 736213.685690394;
+fctd_names(30).st = 736213.725137384;       fctd_names(30).et = 736213.981323727;
+fctd_names(31).st = 736214.029512153;       fctd_names(31).et = 736214.446225347;
+fctd_names(32).st = 736214.577271065;       fctd_names(32).et = 736214.872804977;
+fctd_names(33).st = 736214.908957986;       fctd_names(33).et =  736215.231322222;
+
+fctd_names(34).st = 736215.258631119;         fctd_names(34).et = 736215.361229003;
+fctd_names(35).st = 736215.606821746;         fctd_names(35).et = 736215.737377545;
+fctd_names(36).st = 736215.751948453;         fctd_names(36).et = 736215.893639271;
+fctd_names(37).st = 736215.901516669;         fctd_names(37).et = 736216.003791902;
+fctd_names(38).st = 736216.092956512;         fctd_names(38).et = 736216.201375455;
+fctd_names(39).st = 736216.371579541;         fctd_names(39).et = 736216.439616054;
+
+
+fctd_names(40).st =  736216.614755897;         fctd_names(40).et =  736216.702341582;
+fctd_names(41).st =   736216.703906355;         fctd_names(41).et = 736216.841196537;
+fctd_names(42).st =  736216.841196537;         fctd_names(42).et = 736216.991801055;
+fctd_names(43).st = 736216.991801055;         fctd_names(43).et = 736217.124880162;
+fctd_names(44).st = 736217.140452651;         fctd_names(44).et = 736217.2315509394;
+
+fctd_names(45).st=736222.829623973;
+fctd_names(45).et=736224.228012745;

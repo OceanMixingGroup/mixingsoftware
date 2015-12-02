436e640af55b013ccd7230c553baaf936d441f61
andypicke <andypicke@gmail.com>
2015-12-02 16:20:11 -0700

    lower min temp (good  Arctic data cut off)

--- ctd_processing/ctdpreen.m
+++ ctd_processing/ctdpreen.m
@@ -5,11 +5,13 @@ function d = ctdpreen(d)
 % remove spikes in p t1 t2 t2 c1 c2
 % and oxygen trans fl in volts
 %
+%-------------------
+% 11/12/15 - AP - lower t limit (looking at Arctic data!)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 d.p = preen(d.p, 0, 6200);
-d.t1 = preen(d.t1, 0, 35);
-d.t2 = preen(d.t2, 0, 35);
+d.t1 = preen(d.t1, -3, 35);
+d.t2 = preen(d.t2, -3, 35);
 d.c1 = preen(d.c1, 2.5, 6);
 d.c2 = preen(d.c2, 2.5, 6);
 d.oxygen = preen(d.oxygen, 0.5, 3.5); % volts

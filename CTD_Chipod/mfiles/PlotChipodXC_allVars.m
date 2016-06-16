9d87a75ca84b89f4e54237caa179e84d318ce1aa
andypicke <andypicke@gmail.com>
2016-06-16 10:50:40 -0600

    add wiseness and ivar input options

--- CTD_Chipod/mfiles/PlotChipodXC_allVars.m
+++ CTD_Chipod/mfiles/PlotChipodXC_allVars.m
@@ -1,16 +1,35 @@
-function ax=PlotChipodXC_allVars(XC,whSN,castdir)
+function ax=PlotChipodXC_allVars(XC,whSN,castdir,whsens,xvar)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %
 % General function to plot dT/dz,N2,chi,eps,and KT for one chipod, from sructure 'XC' of chipod
 % profiles.
 %
+% INPUT
+% - XC : Structure made w/  Make_Combined_Chi_Struct_Template.m
+% - whSN
+% - castdir : 'up' or 'down'
+% - whsens : 'T1' or 'T2'
+% - xvar : 'dnum' or 'lat'
+%
+% OUPUT
+% - ax : Axes handles to plot
+%
 %---------------------
 % 01/05/16 - A. Pickering - apickering@coas.oregonstate.edu
+% 06/13/16 - AP - Add 'whsens' to inputs.
+%          - AP - Simplify code w/ X
+%          - AP - Add xvar to inputs
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
-
 %yl=[0 5500];
+
+% Save some typing later on...
+X=XC.([whSN '_' castdir '_' whsens]);
+
+% Reversing lat and dnum messes up plots; eventually should sort
+X.dnum(find(diffs(X.dnum)<0)+1)=nan;
+X.lat(find(diffs(X.dnum)<0)+1)=nan;
 
 figure(1);clf
 agutwocolumn(1)
@@ -18,25 +37,21 @@ wysiwyg
 ax = MySubplot(0.1, 0.05, 0.02, 0.06, 0.1, 0.02, 1,5);
 
 axes(ax(1))
-ezpc(XC.([whSN '_' castdir]).lat,XC.([whSN '_' castdir]).P,log10(XC.([whSN '_' castdir]).N2) );
+ezpc(X.(xvar),X.P,log10(X.N2) );
 hold on
 cb=colorbar;
 cb.Label.String='log_{10}N^2 [s^{-2}]';
 cb.Label.FontSize=14;
 hold on
-plot(XC.([whSN '_' castdir]).lat,0,'kp')
-%    xlim(xl)
-%    ylim(yl)
+plot(X.(xvar),0,'kp')
 xtloff
-title([XC.Name  ' \chi -pod ' whSN ' ' castdir 'casts'],'interpreter','none')
+title([XC.Name  ' \chi -pod ' whSN ' ' castdir 'casts'])%,'interpreter','none')
 SubplotLetterMW('N^2');
 
 %
 axes(ax(2))
-ezpc(XC.([whSN '_' castdir]).lat,XC.([whSN '_' castdir]).P,log10(XC.([whSN '_' castdir]).dTdz));
+ezpc(X.(xvar),X.P,log10(X.dTdz));
 hold on
-%    xlim(xl)
-%    ylim(yl)
 xtloff
 SubplotLetterMW('dT/dz');
 cb=colorbar;
@@ -45,11 +60,9 @@ cb.Label.FontSize=14;
 
 %
 axes(ax(3))
-ezpc(XC.([whSN '_' castdir]).lat,XC.([whSN '_' castdir]).P,log10(XC.([whSN '_' castdir]).chi));
+ezpc(X.(xvar),X.P,log10(X.chi));
 hold on
 caxis([-11 -7])
-%    xlim(xl)
-%    ylim(yl)
 xtloff
 ylabel('Pressure [db]')
 SubplotLetterMW('chi');
@@ -59,29 +72,28 @@ cb.Label.FontSize=14;
 
 %
 axes(ax(4))
-ezpc(XC.([whSN '_' castdir]).lat,XC.([whSN '_' castdir]).P,log10(XC.([whSN '_' castdir]).eps));
+ezpc(X.(xvar),X.P,log10(X.eps));
 hold on
 cb=colorbar;
 cb.Label.String='log_{10}\epsilon [Wkg^{-1}]';
 cb.Label.FontSize=14;
 caxis([-11 -6])
-%    xlim(xl)
-%    ylim(yl)
 xtloff
 SubplotLetterMW('eps');
 
 %
 axes(ax(5))
-ezpc(XC.([whSN '_' castdir]).lat,XC.([whSN '_' castdir]).P,log10(XC.([whSN '_' castdir]).KT));
+ezpc(X.(xvar),X.P,log10(X.KT));
 cb=colorbar;
 cb.Label.String='log_{10}K_T';
 cb.Label.FontSize=14;
 caxis([-8 -1])
 hold on
-%    xlim(xl)
-%    ylim(yl)
-xlabel('Latitude','fontsize',16)
+xlabel(xvar,'fontsize',16)
 SubplotLetterMW('K_T');
+if strcmp(xvar,'dnum')
+    datetick('x')
+end
 
 linkaxes(ax)
 

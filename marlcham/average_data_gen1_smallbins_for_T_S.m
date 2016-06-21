51cbdfd02bff6acfc3cfde66d42cc227e4fec2de
Sally Warner <sally.warner@gmail.com>
2016-06-21 15:31:34 -0700

    created new version of chameleon processing that can make 10cm vertical bins
    For Concorde16, want to process T, S and all other non-turbulence
    variables into 10cm bins rather than 1m vertical bins.

---
+++ marlcham/average_data_gen1_smallbins_for_T_S.m
@@ -0,0 +1,198 @@
+function avg=average_data_gen1_smallbins_for_T_S(series,varargin)
+% function AVG=AVERAGE_DATA(SERIES,PROPERTY1,VALUE1,PROPERTY2,VALUE2,...)
+% This function averages variables from the structure CAL and returns them
+% in the structure AVG.  SERIES may be any of the CAL fieldnames (P,TP for
+% example) or epsilon1 (from S1), epsilon2 (from S2) or chi1 (from
+% TP or TP1 or T1P) or chiXXX (from XXX)
+% and SERIES is a cell array (eg. series={'T1','EPSILON1','AX','UC'})
+% The current properties and their defaults are:
+% min_bin=0; % first depth or time bin
+% max_bin=inf; % final depth or time bin
+% binsize=1; % in m for P, or seconds for TIME, or whatever.
+% depth_or_time='P' % this is the cal.?? series to be used for binning.
+% nfft=256; % number of slow-sampled data points in each averaging interval
+% whole_bins=0; % set to 1 for bins spaced at 1m, 2m instead of 0.5m, 1.5m etc.
+% epsilon_glitch_factor=6; % the ratio of EPSILON1/EPSILON2 which, if
+%                            exceeded, selects the smaller epsilon for avg.EPSILON
+%
+% Example:  avg=average_data({'T1','CHI1','EPSILON2'},'min_bin',15, ...
+%          'depth_or_time','P2','nfft',128)
+% 
+% will average cal.T1, calculate chi_t from cal.T1, and calculate epsilon 
+% from cal.S2 over the range 15m-bottom.  Spectra are calculated using
+% 128*head.irep.SERIES points for each of the SERIES S1 and T1.  Each
+% segment is overlapped by 50%.  The series cal.P2 is used for binning.  To
+% use time for binning, run make_time_series and set the property 
+% 'depth_or_time' to 'time'.
+%
+% NOTE that 'FALLSPD' MUST preceed 'EPSILON' and 
+% 'EPSILON?' must preceed 'CHI' calculations
+%
+% also note that there must be at least nfft/16 points in each bin.
+%
+% This script differs from average_data.m
+% It uses calc_epsilon_filt_gen.m routine to calculate EPSILON
+% for spiked shear data without losing variance
+% The script calls set_filters_{preffix}.m file where all filters and coefficiants are
+% defined.
+%******************************************************************
+% ********* AN EXAMPLE OF SET_FILTERS_hm00.M FILE **********************
+%    % set_filters.m
+%    % A suppliment file for average_data_gen.
+%    % Here you can set filters that will be used to calculate
+%    % EPSILON in calc_epsilon_filt_gen.m routine.
+%    % In addition high and low frequency cutoff and high frequency 
+%    % cutoff coefficient are defined here (see documentation to 
+%    % calc_epsilon_filt_gen.m).
+%    % Choice of filters and coefficients depends on cast number
+%    % (it could be different for different tows)
+%    % Note: variable q (for cast number) should be defined as GLOBAL
+%    % in main script and in average_data_gen.m routine
+%    %$$$$$$$$ DO NOT CHANGE THIS SECTION!! $$$$$$$$$$$$$$$$$$$$$
+%    global q
+%    clear filters;
+%    % SET DEFAULTS
+%    filters={};
+%    k_start=2; % [cpm]
+%    k_stop=90; % [cpm]
+%    kstop_coef=0.5;
+%    eps=eval(['avg.EPSILON' prb '(n)']);
+%    %$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+%   
+%    %##### IN THIS SECTION YOU SET FILTERS AND COEFFICIENTS ####
+%    %##### AS FUNCTION OF CAST NUMBER ##########################
+%    % Filters set according to EPSILON estimation (variable eps)
+%    % EPSILON estimated in average_data_gen script before applying filters
+%    % Filters could be different for diferent probes (variable prb)
+%    % and could depend on ADP (variable cal.FLAG, cal.FLAG(:)=1 when 
+%    % ADP is ON)
+%    cast=q.script.num;
+%    k_start=1; % [cpm]
+%    if cast>=194 & cast<=407 
+%        if eps<3e-8 % filters set according to EPSILON estimation
+%            if cal.FLAG(1)==1 % cal.FLAG=1 when ADP was turned ON
+%                              % filters depend on ADP
+%                if prb==1 | prb==2 % prb is the probe number. Filters could
+%                                   % be different for different probes
+%                    filters={'n2.4-6.8','l12'};
+%                else
+%                    filters={'n3-8','l12'};
+%                end
+%            else % ADP turned OFF
+%                if prb==1 | prb==2 
+%                    filters={'n2.4-6.8','n12-18','n27-32'};
+%                else
+%                    filters={'n3-8','n12-18','n27-32'};
+%                end
+%            end
+%        elseif eps<8e-8
+%            if prb==1 | prb==2
+%                filters={'n2.4-6.8'};
+%            else
+%                filters={'n3-8'};
+%            end
+%        end
+%    elseif cast>=417 & cast<=592
+%        % TOW #2
+%        if eps<3e-8 % filters set according to EPSILON estimation
+%            if cal.FLAG(1)==1
+%                filters={'l11'};
+%            else % ADP turned OFF
+%                filters={'n11-21','n26.6-32.5'};
+%            end
+%        elseif eps<8e-8
+%            filters={'n11-21','n26.5-32.5'};
+%        end
+%    end
+%****** END OF EXAMPLE *************************************************
+%***********************************************************************
+
+%% notes: SJW, March 2015
+% Updating this code to work with the new chameleon data.
+
+
+%% notes: SJW, June 2016
+% Want an additional function that will calculate all the variables except
+% epsilon and chi over smaller bins, say 10cm, for a shallow deployment
+% like Concorde16
+
+
+%% define some parameters (some of these will be overwritten with varargin)
+
+min_bin=0; % first depth bin
+max_bin=inf; % final depth bin 
+depth_or_time='P'; % this is the cal.?? series to be used for binning.
+whole_bins=0; % set to 1 for bins spaced at 1m, 2m instead of 0.5m, 1.5m etc.
+binsize=0.1; % in m
+nfft=256; % number of slow-sampled data points for an epsilon calculation
+epsilon_glitch_factor=6; % the ratio of EPSILON1/EPSILON2 which, if
+%                          % exceeded, selects the smaller epsilon for avg.EPSILON
+% overlap=128; % desired number of points to overlap
+
+global head cal q
+
+%% Redefine any of the above defaults if they have been specified:
+
+% check that inputs are properly paired
+a=length(varargin)/2;
+if a~=floor(a)
+  error('must have matching number of property-pairs')
+  return
+end
+
+% replace parameters with varargin values
+for i=1:2:a*2
+  tmp=varargin(i+1);
+  if strcmp(lower(char(varargin(i))),'depth_or_time')
+    eval(['depth_or_time=''' char(tmp) ''';' ])
+  else
+    eval([lower(char(varargin(i))) '=' num2str(tmp{:}) ';' ])
+  end
+end
+
+% set the series for binning:  
+eval(['binseries=cal.' upper(depth_or_time) ';'])
+eval(['series={''' depth_or_time ''' series{:}};'])
+
+%% now do the averaging over each 1m bin
+
+firstbin=binsize*max(floor(min(binseries)/binsize),min_bin/binsize)+(whole_bins~=0)*binsize/2;
+lastbin=binsize*min(ceil(max(binseries)/binsize),max_bin/binsize)-(whole_bins~=0)*binsize/2;
+
+avail_series=fieldnames(cal);
+n=0;
+% find the indices of each meter's worth of data
+for pressure=firstbin:binsize:(lastbin-binsize)
+  temp=find(binseries>pressure & binseries<(pressure+binsize));
+  n=n+1;
+  if ~isempty(temp)
+     min_ind(n)=temp(1);
+     max_ind(n)=temp(length(temp));
+     % the following makes it so that data is not given if there are less than
+     % nfft/16 data points in the series
+%      n=n-((max_ind(n)-min_ind(n))<(nfft/16));
+  else
+     n=n-1;
+  end
+end
+nmax=n;
+
+% go through every variable in 'series' and either average the variable or 
+% calculate epsilon or chi
+for i=1:length(series)
+    active=upper(char(series(i)));
+    if strncmp(active,'EPSILON',7) 
+        avg.EPSILON(1:nmax)=NaN;
+    elseif strncmp(active,'CHI',3) 
+        avg.CHI(1:nmax)=NaN;
+        
+    else % average data that is NOT epsilon or chi
+        for n=1:nmax
+            eval(['avg.' active '(n)=mean(cal.' active ...
+                    '(1+(min_ind(n)-1)*head.irep.' active ...
+                    ':max_ind(n)*head.irep.' active '));']);
+        end
+    end
+end
+
+

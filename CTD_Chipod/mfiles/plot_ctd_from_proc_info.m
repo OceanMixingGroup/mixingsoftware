e7584966bb3e1ec502ba27d27231c9d9abdd712c
andypicke <andypicke@gmail.com>
2016-09-19 12:51:03 -0600

    change function name

--- CTD_Chipod/mfiles/plot_ctd_from_proc_info.m
+++ CTD_Chipod/mfiles/plot_ctd_from_proc_info.m
@@ -1,12 +1,12 @@
-function ax=plot_ctd_from_xc(XC)
+function ax=plot_ctd_from_proc_info(proc_info)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %
-% ax=plot_ctd_from_xc(XC)
+% ax=plot_ctd_from_xc(proc_info)
 %
 % Make a generic pcolor plot of CTD temp and sal for a CTD-chipod cruise
 %
 % INPUT
-%  XC - structure w/ fields
+%  proc_info - structure w/ fields
 %       - ctd.t
 %       - ctd.s
 %       - ctd.p
@@ -27,21 +27,19 @@ wysiwyg
 set(gcf,'DefaultAxesFontsize',14)
 
 ax1=subplot(211);
-ezpc(XC.lat,XC.ctd.p,XC.ctd.t);
+ezpc(proc_info.lat,proc_info.ctd.p,proc_info.ctd.t);
 hold on
-%contour(proc_info.lat,zout,t,[0:1:30],'k')
-%plot(proc_info.lat,zmax,'ko')
 cb=colorbar;
 cb.Label.String='^oC';
-ylim([0 nanmax(XC.ctd.p)])
+ylim([0 nanmax(proc_info.ctd.p)])
 xlabel('Latitude','fontsize',16)
 ylabel('P [db]','fontsize',16)
 
 ax2=subplot(212);
-ezpc(XC.lat,XC.ctd.p,XC.ctd.s);
+ezpc(proc_info.lat,proc_info.ctd.p,proc_info.ctd.s);
 cb=colorbar;
 cb.Label.String='psu';
-ylim([0 nanmax(XC.ctd.p)])
+ylim([0 nanmax(proc_info.ctd.p)])
 colormap(gca,salmap);
 xlabel('Latitude','fontsize',16)
 ylabel('P [db] ','fontsize',16)

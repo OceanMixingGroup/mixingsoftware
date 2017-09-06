791f6ef449d9719fa03d61e474633720aef8c84c
andypicke <andypicke@gmail.com>
2017-09-06 10:12:40 -0600

    update function to write latest notes

--- CTD_Chipod/mfiles/Write_Latex_Notes.m
+++ CTD_Chipod/mfiles/Write_Latex_Notes.m
@@ -26,7 +26,7 @@ eval(['Chipod_Deploy_Info_' Project])
 
 %% open a text file
 
-txtfname=['Chipod_Note_' project '_test.tex'];
+txtfname=fullfile(cruise_path,Project,'Notes',['Chipod_Note_' Project '_auto.tex']);
 
 fileID= fopen(fullfile(txtfname),'w');
 %%
@@ -56,19 +56,20 @@ fprintf(fileID,'\\DeclareGraphicsRule{.tif}{png}{.png}{`convert #1 `dirname #1`/
 
 %*** Set graphics path for figures
 fprintf(fileID,'\\graphicspath{\n')
-fprintf(fileID,['{/Users/Andy/Cruises_Research/Chipod/' project '/}\n'])
-fprintf(fileID,['{/Users/Andy/Cruises_Research/Chipod/' project '/Figures/}\n'])
+fprintf(fileID,['{' fullfile(cruise_path,Project) '/}\n'])
+fprintf(fileID,['{' fullfile(cruise_path,Project,'figures') '/}\n'])
+fprintf(fileID,['{' fullfile(cruise_path,Project,'figures','XC') '/}\n'])
 
-% need to loop through and add path to each SN here
-
+% loop through and add path to each SN here
 for iSN=1:length(ChiInfo.SNs)
-    fprintf(fileID,['{/Users/Andy/Cruises_Research/ChiPod/' project '/Data/proc/Chipod/SN' ChiInfo.SNs{iSN} '/figures/}\n'])
+    fprintf(fileID,['{/Users/Andy/Cruises_Research/ChiPod/' Project '/Data/proc/Chipod/SN' ChiInfo.SNs{iSN} '/figures/}\n'])
+    fprintf(fileID,['{' fullfile(cruise_path,Project,'figures',['SN' ChiInfo.SNs{iSN}]) '/}\n'])
 end
 fprintf(fileID,'} \n\n')
 
 %% Begin document
 
-fprintf(fileID,[ '\\title{' project ' CTD-Chipod Notes} \n'])
+fprintf(fileID,[ '\\title{' Project ' CTD-Chipod Notes} \n'])
 fprintf(fileID, '\\author{Andy Pickering} \n')
 %\date{}                                           % Activate to display a given date or no date
 
@@ -80,39 +81,47 @@ fprintf(fileID, '\\tableofcontents \n')
 fprintf(fileID, '\\newpage \n\n')
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-fprintf(fileID, '\\section{About} \n\n')
+fprintf(fileID,'%s',['%~~~~~~~~~~~~~~~~~~~~~~~~~~']);
+fprintf(fileID,'\n \\section{About} \n\n');
 
-fprintf(fileID, ['Notes on processing and analysis of CTD-chipod data collected during ' project ' cruise.. \n\n'])
+fprintf(fileID, ['Notes on processing and analysis of CTD-chipod data collected during ' Project ' cruise.. \n\n'])
 
 fprintf(fileID, '\\begin{figure}[htbp] \n')
-fprintf(fileID, ['\\includegraphics[width=38pc]{' project '_kml_map.jpg} \n'])
-fprintf(fileID, ['\\caption{Map of CTD cast locations during cruise ' project '.} \n'])
+fprintf(fileID, ['\\includegraphics[width=38pc]{' Project '_kml_map.jpg} \n'])
+fprintf(fileID, ['\\caption{Map of CTD cast locations during cruise ' Project '.} \n'])
 fprintf(fileID, '\\label{map} \n')
 fprintf(fileID, '\\end{figure} \n\n')
 
 
+
+
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-fprintf(fileID, '\\section{Data and Processing} \n\n')
+fprintf(fileID,'\n\n%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+fprintf(fileID,'\\section{Data and Processing} \n\n')
 
 fprintf(fileID, 'Data paths are set w/ :  \n')
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item \verb+Load_chipod_paths_' project '.m+ \n'])
+fprintf(fileID, ['\\item \\verb+Load_chipod_paths_' Project '.m+ \n'])
 fprintf(fileID, '\\end{itemize} \n\n')
 
 fprintf(fileID, 'Chipod deployment info is given in  \n')
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item \verb+Chipod_Deploy_Info_' project '+ \n'])
+fprintf(fileID, ['\\item \\verb+Chipod_Deploy_Info_' Project '+ \n'])
 fprintf(fileID, '\\end{itemize} \n\n')
 
-fprintf(fileID, '%~~~~~~~~ \n\n')
+
+
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsection{CTD} \n\n')
 
 fprintf(fileID, 'Raw (hex) CTD data are processed with: \n')
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item \verb+Process_CTD_hex_' project '.m+ \n'])
+fprintf(fileID, ['\\item \\verb+Process_CTD_hex_' Project '.m+ \n'])
 fprintf(fileID, '\\end{itemize} \n\n')
 
-fprintf(fileID, '%~~~~~~~~ \n\n')
+
+
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsection{Chipod} \n\n')
 
 fprintf(fileID, '% **** Made w/ MakeTableChiDeploy \n')
@@ -129,7 +138,7 @@ fprintf(fileID, '\\end{table}% \n\n')
 
 fprintf(fileID, 'Raw chipod files are plotted with  \n')
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item \verb+PlotChipodDataRaw_' project '.m+ \n'])
+fprintf(fileID, ['\\item \\verb+PlotChipodDataRaw_' Project '.m+ \n'])
 fprintf(fileID, '\\end{itemize} \n')
 fprintf(fileID, 'to check for obvious issues/malfunctions with any of the instruments; these plots are saved in \verb+/Figures/chipodraw/+.  \n')
 
@@ -143,22 +152,24 @@ fprintf(fileID, 'Files with obviously bad or missing data (based on above raw pl
 
 fprintf(fileID, 'Processing scripts are: \n')
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item \verb+MakeCasts_CTDchipod_' project '.m+ \n'])
-fprintf(fileID, ['\\item \verb+DoChiCalc_' project '.m+ \n'])
+fprintf(fileID, ['\\item \\verb+MakeCasts_CTDchipod_' Project '.m+ \n'])
+fprintf(fileID, ['\\item \\verb+DoChiCalc_' Project '.m+ \n'])
 fprintf(fileID, '\\end{itemize} \n\n')
 
-fprintf(fileID, '%~~~~~~~~ \n\n')
+
+
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsection{Further processing and analysis} \n\n')
 
 fprintf(fileID, '\\begin{itemize} \n')
-fprintf(fileID, ['\\item Data from all casts are combined into a single structure w/ \verb+MakeCombinedStruct_' project '.m+ \n'])
+fprintf(fileID, ['\\item Data from all casts are combined into a single structure w/ \verb+MakeCombinedStruct_' Project '.m+ \n'])
 fprintf(fileID, '\\item  \n')
 fprintf(fileID, '\\end{itemize} \n\n')
 
 
 fprintf(fileID, '\\newpage \n\n')
-fprintf(fileID, '%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n')
-fprintf(fileID, '\\section{Processing Notes} \n\n')
+fprintf(fileID,'%s',['%~~~~~~~~~~~~~~~~~~~~~~~~~~']);
+fprintf(fileID, '\n \\section{Processing Notes} \n\n')
 
 fprintf(fileID, '\\begin{itemize} \n')
 fprintf(fileID, '\\item Table \ref{chidepinfo} gives the info for $\chi$pods deployed. \n')
@@ -174,11 +185,12 @@ fprintf(fileID, '%% make and insert table 2 \n\n')
 fprintf(fileID, '%% continue... \n\n')
 
 fprintf(fileID, '%~~ \n\n')
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsubsection{Time Offsets} \n\n')
 
 
 fprintf(fileID, '\\begin{figure}[htbp] \n\n')
-fprintf(fileID, ['\\includegraphics[scale=1]{' project '_timeoffsets_all.png} \n'])
+fprintf(fileID, ['\\includegraphics[scale=1]{' Project '_timeoffsets_all.png} \n'])
 fprintf(fileID, '\\caption{Time-offsets for all $\\chi$pods, found by aligning with CTD data.} \n')
 fprintf(fileID, '\\label{toffs} \n')
 fprintf(fileID, '\\end{figure} \n\n')
@@ -186,8 +198,8 @@ fprintf(fileID, '\\end{figure} \n\n')
 
 
 fprintf(fileID, '\\newpage \n\n')
-fprintf(fileID, '%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n')
-fprintf(fileID, '\\section{Example Raw Chipod Data for 1 cast} \n\n')
+fprintf(fileID,'%s',['%~~~~~~~~~~~~~~~~~~~~~~~~~~']);
+fprintf(fileID, '\n \\section{Example Raw Chipod Data for 1 cast} \n\n')
 
 for iSN=1:length(ChiInfo.SNs)
     
@@ -201,17 +213,17 @@ end
 %%
 
 fprintf(fileID, '\\newpage \n\n')
-fprintf(fileID, '%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n')
+fprintf(fileID,'\n\n%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 fprintf(fileID, '\\section{Results} \n\n')
 
 
 fprintf(fileID, '\\clearpage \n\n')
-%~~
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsubsection{CTD Data } \n')
 
 fprintf(fileID, '% Figure showing pcolor of t and s from all casts \n')
 fprintf(fileID, '\\begin{figure}[htbp] \n')
-fprintf(fileID, ['\\includegraphics[scale=0.7]{' project '_ctd_t_s.png} \n'])
+fprintf(fileID, ['\\includegraphics[scale=0.7]{' Project '_ctd_t_s.png} \n'])
 fprintf(fileID, '\\caption{Plot of temperature and salinity from CTD downcasts on all casts.} \n')
 fprintf(fileID, '\\label{} \n')
 fprintf(fileID, '\\end{figure} \n\n')
@@ -219,14 +231,18 @@ fprintf(fileID, '\\end{figure} \n\n')
 
 fprintf(fileID, '\\clearpage \n\n')
 %~~
+
+
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsubsection{Data from each individual Chipod} \n\n')
 
 % LOOP over SNs
 % *** One for each chipod
 for iSN=1:length(ChiInfo.SNs)
+    whSN=ChiInfo.SNs{iSN};
     fprintf(fileID, '\\begin{figure}[htbp] \n')
-    fprintf(fileID, ['\\includegraphics[scale=0.7]{XC_SN' ChiInfo.SNs{iSN} '_Vs_lat_AllVars.png} \n'])
-    fprintf(fileID, ['\\caption{All chipod profiles from sensor SN' ChiInfo.SNs{iSN} '. Variables are: N2, dTdz, chi, eps, and KT.} \n'])
+    fprintf(fileID, ['\\includegraphics[scale=0.7]{XC_' whSN '_Vs_lat_' ChiInfo.(whSN).InstDir.T1 'AllVars.png} \n'])
+    fprintf(fileID, ['\\caption{All chipod profiles from sensor ' whSN '. Variables are: N2, dTdz, chi, eps, and KT.} \n'])
     fprintf(fileID, '\\label{} \n')
     fprintf(fileID, '\\end{figure} \n\n')
 end
@@ -234,21 +250,24 @@ end
 fprintf(fileID, '\\clearpage \n')
 fprintf(fileID, '\\newpage \n')
 %~~
+
+
+fprintf(fileID, '$s', '%~~~~~~~~~~~~ \n\n')
 fprintf(fileID, '\\subsubsection{One variable from all Chipods} \n\n')
 
 fprintf(fileID, '\\begin{figure}[htbp] \n')
-fprintf(fileID, ['\\includegraphics[scale=0.7]{' project '_chi_AllSNs_Vslat.png} \n'])
+fprintf(fileID, ['\\includegraphics[scale=0.7]{' Project '_chi_AllSNs_Vslat.png} \n'])
 fprintf(fileID, '\\caption{Plot of one variable from all chipods.} \n')
 fprintf(fileID, '\\label{} \n')
 fprintf(fileID, '\\end{figure} \n\n')
 
 fprintf(fileID, '\\begin{figure}[htbp] \n')
-fprintf(fileID, ['\\includegraphics[scale=0.7]{' project '_KT_AllSNs_Vslat.png} \n'])
+fprintf(fileID, ['\\includegraphics[scale=0.7]{' Project '_KT_AllSNs_Vslat.png} \n'])
 fprintf(fileID, '\\caption{Plot of one variable from all chipods.} \n')
 fprintf(fileID, '\\label{} \n')
 fprintf(fileID, '\\end{figure} \n\n')
 
 
 fprintf(fileID, '\\end{document}  \n')
-
+fprintf(fileID,'\n\n%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 %%

1150a246f68bce927de1331ad90056183c852b1f
andypicke <andypicke@gmail.com>
2017-09-08 09:41:21 -0600

    add option to pass in Params

--- CTD_Chipod/mfiles/do_chi_calc_ctd_chipod.m
+++ CTD_Chipod/mfiles/do_chi_calc_ctd_chipod.m
@@ -1,4 +1,4 @@
-function do_chi_calc_ctd_chipod(the_project,mixpath)
+function do_chi_calc_ctd_chipod(the_project,mixpath,varargin)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %
 % Function version of DoChiCalc_Template.m
@@ -35,6 +35,15 @@ function do_chi_calc_ctd_chipod(the_project,mixpath)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
+% Check number of inputs.
+if length(varargin) > 1
+    error('requires at most 1 optional inputs');
+end
+
+% Add paths we need from mixing software
+addpath(fullfile(mixpath,'CTD_Chipod','mfiles')) ;
+addpath(fullfile(mixpath,'chipod','compute_chi')); % get_chipod_chi.md
+
 this_script_name = 'do_chi_calc_ctd_chipod.m' ;
 
 % Load paths for CTD and chipod data
@@ -45,23 +54,14 @@ eval(['Chipod_Deploy_Info_' the_project])
 
 savespec = 0 ; % Option to save spectra
 
-%~~ set some params for following calcs
-do_T2_big       = 1  ;  % do calc for T2 if big chipod
-Params.z_smooth = 20 ;  % distance (m) over which to smooth N^2 and dT/dz
-Params.nfft     = 128;  % nfft to use in computing wavenumber spectra
-Params.extra_z  = 2  ;  % number of extra meters to get rid of due to CTD pressure loops.
-Params.wthresh  = 0.3;  % w threshold for removing CTD pressure loops
-Params.TPthresh = 1e-6 ; % minimum TP variance to do calculation
-Params.fmax     = 7  ;  % max freq to integrate TP spectrum to in chi calc
-Params.resp_corr= 0  ;  % correct TP spectra for freq response of thermistor
-Params.fc       = 99 ;  % cutoff frequency for response correction
-Params.gamma    = 0.2;  % mixing efficiency
-%~~
-%~~~~~~~~~~~~~~~~~~~~~
+if length(varargin)==1
+    Params = varargin{1} ;
+else
+    disp('using default Params');
+    Params = SetDefaultChiParams ;
+end
+% if Params not passed in, use defaults
 
-% Add paths we need from mixing software
-addpath(fullfile(mixpath,'CTD_Chipod','mfiles')) ;
-addpath(fullfile(mixpath,'chipod','compute_chi')); % get_chipod_chi.md
 
 tstart=tic;
 

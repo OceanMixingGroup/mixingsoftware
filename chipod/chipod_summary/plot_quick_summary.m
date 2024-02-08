008e0f12ceed08858a02ba4798a3aa9752e34151
Ken Hughes <hugke729@gmail.com>
2024-02-08 11:36:10 -0800

    Add files via upload

--- chipod/chipod_summary/plot_quick_summary.m
+++ chipod/chipod_summary/plot_quick_summary.m
@@ -11,20 +11,21 @@ function plot_quick_summary(ichpd,location,depth)
 %       plot_quick_summary(1109,'TAO 2019 [0,140W]','69');
 
 %Change the parameters that are commented in BOLD
-%This also calls quick_ticks (internal) and subaxis functions that should be in the same path
+%This also calls quick_ticks.m function that should be in the same path
 %Pavan Vutukur 
 %Ocean Mixing Group
 
 %CHANGE THE ROOT FOLDER TO THE DEPLOYMENT NAME
-root = 'E:\chipods\hornbaki_2019\new_h\';
+root = 'Y:\Chipod\sw_reg_2023\long_term_container_test\1001\';
 
 fnm =  [root,num2str(ichpd),'_quick_summary.mat'];
 load(fnm); %loads the summary file
 
 %CHANGE START AND END DATES OF DEPLOYMENT
-t.t1 = datenum('13-aug-2019 00:00:01','dd-mmm-yyyy HH:MM:SS');
-t.t2 = datenum('09-aug-2020 23:59:59','dd-mmm-yyyy HH:MM:SS');
-
+t.t1 = datenum('10-July-2023 00:00:01','dd-mmm-yyyy HH:MM:SS');
+t.t2 = datenum('04-Sep-2023 23:59:59','dd-mmm-yyyy HH:MM:SS');
+% t.t1 = avg.time(1);
+% t.t2 = avg.time(end);
 labl=(['Chipod # ',num2str(ichpd),' / Location: ',location,...
     ' / Target Depth: ',depth,' m /  ','Deployed: ',...
     datestr(t.t1,'dd-mmm-yy'),' / ','Recovered: ',datestr(t.t2,'dd-mmm-yy')]);
@@ -37,7 +38,7 @@ subaxis(8,1,1,'SpacingVert',0,'ML',0.05,'MR',00.02,'MT',0.08);
 atm = 14.29;
 plot(avg.time,(avg.P-atm)./1.47,'b')
 %CHANGE THE LIMITS AND TICKS 
-quick_ticks(t,220,222,220.25,221.75,0.75);%ylim1,ylim2,ytick1,ytick2,tick_div
+quick_ticks(t,0,20,1,19,2);%ylim1,ylim2,ytick1,ytick2,tick_div
 top_ax = gca;
 taX= top_ax.XAxis;
 set(taX,'tickdir','in');
@@ -49,7 +50,7 @@ title(['Summary Plot for ',labl]);
 subaxis(8,1,2,'SpacingVert',0,'ML',0.05,'MR',0.02,'MB',0.08);
 plot(avg.time,avg.AX,'k',avg.time,avg.AY,'r');
 %CHANGE THE LIMITS AND TICKS
-quick_ticks(t,-6,6,-4,4,4); %ylim1,ylim2,ytick1,ytick2,tick_div
+quick_ticks(t,-20,20,-15,15,7.5); %ylim1,ylim2,ytick1,ytick2,tick_div
 legend('Ax','Ay','Location','northeast'); legend boxoff;
 ylabel('A_{XY} (Tilt^\circ)');
 
@@ -58,14 +59,14 @@ ylabel('A_{XY} (Tilt^\circ)');
 subaxis(8,1,3,'SpacingVert',0,'ML',0.05,'MR',0.02);
 plot(avg.time,avg.AZ,'b');
 %CHANGE THE LIMITS AND TICKS
-quick_ticks(t,-1.01,-0.98,-1.005,-0.985,0.01);%ylim1,ylim2,ytick1,ytick2,tick_div
-ylabel('A_Z (g_{force})')
+quick_ticks(t,-1.03,-0.94,-1.01,-0.95,0.02);%ylim1,ylim2,ytick1,ytick2,tick_div
+ylabel('A_Z (g_{force})');
 
 %Plot Chipod T1 and T2 in Celsius
 subaxis(8,1,4,'SpacingVert',0,'ML',0.05,'MR',0.02);
 plot(avg.time,avg.T1,'k',avg.time,avg.T2,'r');
 %CHANGE THE LIMITS AND TICKS
-quick_ticks(t,0,8,2,6,2); %ylim1,ylim2,ytick1,ytick2,tick_div
+quick_ticks(t,14,60,15,39,5); %ylim1,ylim2,ytick1,ytick2,tick_div
 if isfield(avg,'sensor_id')
     legend(strcat('T1:',{' '},convertCharsToStrings(avg.sensor_id.T1(1,:))), strcat('T2:',{' '},convertCharsToStrings(avg.sensor_id.T2(1,:))),'Location','northeast'); 
     legend boxoff;
@@ -79,7 +80,7 @@ ylabel('T (^\circC)');
 subaxis(8,1,5,'SpacingVert',0,'ML',0.05,'MR',0.02);
 plot(avg.time,avg.T1P,'k',avg.time,avg.T2P,'r'); %Plotting in log scale
 %CHANGE THE LIMITS AND TICKS
-tp_idx = 1e-10; %Just change this value to update TP Limit and Ticks
+tp_idx = 1e-9; %Just change this value to update TP Limit and Ticks
 quick_ticks(t,0,tp_idx,0.1*tp_idx,0.9*tp_idx,0.4*tp_idx);
 %ylim1,ylim2,ytick1,ytick2,tick_div
 legend('T1P','T2P','Location','northeast'); legend boxoff;
@@ -89,15 +90,19 @@ ylabel('TP \sigma^{2} (V)');
 subaxis(8,1,6,'SpacingVert',0,'ML',0.05,'MR',0.02);
 plot(avg.time,avg.W,'b');
 %CHANGE THE LIMITS AND TICKS
-quick_ticks(t,1,1.8,1.1,1.7,0.3);%ylim1,ylim2,ytick1,ytick2,tick_div
+quick_ticks(t,0,4,0.5,3.5,1.5);%ylim1,ylim2,ytick1,ytick2,tick_div
 ylabel('Pitot (V)')
 
-%Plot digital voltage data
+%Plot digital and analog voltage data
 subaxis(8,1,7,'SpacingVert',0,'ML',0.05,'MR',0.02);
 plot(avg.time,(avg.V)./1000,'r');
+hold on;
+%plot(avg.time,(avg.Va+1.8),'k'); % for current limiting diodes
+plot(avg.time,((avg.Va./.5537)+.418),'k'); % for schotky diodes
 %CHANGE THE LIMITS AND TICKS
-quick_ticks(t,7.0,7.25,7.02,7.22,0.1);%ylim1,ylim2,ytick1,ytick2,tick_div
-ylabel({'Digital';'Batt (V)'});
+quick_ticks(t,1,8,1.5,7.5,1.5);%ylim1,ylim2,ytick1,ytick2,tick_div
+ylabel({'Batt (V)'});
+legend('Digital', 'Analog');
 
 %plot compass data
 subaxis(8,1,8,'SpacingVert',0,'ML',0.05,'MR',0.02);
@@ -111,20 +116,6 @@ ylabel('CMP^\circ');
 %name as summary file
 fname=(['summary_',num2str(ichpd)]);
 print([root fname], '-dpng','-r300');
-
-function quick_ticks(t,ylim_1,ylim_2,ytick_1,ytick2,tick_div)
-%This function inputs tick limits and tick divisor for each subplot in make_avg_chipod that are common to every subplot to 
-%reduce the length of code
-	set(gca,'Ylim',[ylim_1,ylim_2]); %Change the Limits to each chipod
-	set(gca,'YTick',(ytick_1:tick_div:ytick2)); %Change the # of YTick 
-	set(gca,'Xlim',[t.t1 t.t2]);
-	set(gca,'FontSize',10, 'FontWeight','Bold');
-	datetick('x','mmm-YYYY','keeplimits');
-	set(gca,'tickdir','out');
-	set(gca,'xticklabel','  ');
-	set(gca,'xgrid','on');
-end
-
 end
 
 
